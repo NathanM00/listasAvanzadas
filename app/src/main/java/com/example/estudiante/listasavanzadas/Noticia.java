87eb70bc30707f307f212c04a619951d5c85f28d
manuelaepedesign <manuelaepedesign@gmail.com>
2018-08-30 13:01:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/estudiante/listasavanzadas/Noticia.java
@@ -0,0 +1,38 @@
+package com.example.estudiante.listasavanzadas;
+
+public class Noticia {
+
+    private String titulo, descripcion,fecha;
+    //private String urlImg;
+
+
+    public Noticia(String titulo, String descripcion, String fecha) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+}
